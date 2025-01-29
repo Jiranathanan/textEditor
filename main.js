f930ff6996229e0c4e24e36d6c3e33fdbd7e6143
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-01-29 20:54:10 +0700

    Intregate node and IPC to send data between renderer and main process

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
-    let win = new BrowserWindow({});
+    let win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
     win.loadFile('index.html');
+})
+
+ipcMain.on('save', (event, text) => {
+    // save the text to a file
+    console.log(text);
 })
